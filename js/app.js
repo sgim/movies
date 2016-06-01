a95be7e232442fc87b01e7d604e4621e8ce78caf
sgim <levin.ab@gmail.com>
2016-06-01 14:15:25 -0400

    quick fix to redirect to page 1

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 "use strict";
 var app = angular.module("SwitchMovies", ["ngMaterial", "ui.router", "ui.bootstrap"]);
+
+app.config(function ($stateProvider) {
+  $stateProvider.state("default", {
+    url: "",
+    controller: function ($state) {
+      $state.go("home");
+    }
+  });
+});
